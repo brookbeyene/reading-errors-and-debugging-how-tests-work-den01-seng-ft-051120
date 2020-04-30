8ae0b59921447ce5093638b8dac00ce3fab0ff31
Brook Beyene <brookbeyene37@gmail.com>
2020-04-30 22:15:48 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,10 @@
 # Add your variables here
+
+first_number = 32
+second_number = 13
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+
+quotient = first_number.div(second_number)
